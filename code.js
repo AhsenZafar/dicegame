242c853e4d9a313704e566a370a19d19a3a4702f
AhsenZafar <100182384+AhsenZafar@users.noreply.github.com>
2022-09-05 00:55:46 -0600

    code functioning but turns need to be made fair

--- code.js
+++ code.js
@@ -1,6 +1,10 @@
+
 let player1Score = 0
 let player2Score = 0
+let p1Turns =0
+let p2Turns =0
 let player1Turn = true
+
 
 const player1Dice = document.getElementById("player1Dice")
 const player2Dice = document.getElementById("player2Dice")
@@ -10,13 +14,58 @@ const message = document.getElementById("message")
 const rollBtn = document.getElementById("rollBtn")
 const resetBtn = document.getElementById("resetBtn")
 
-rollBtn.addEventListener("click", function() {
-    const randomNumber = Math.floor(Math.random() * 6) + 1 // 0-5 -> 1-6
-    console.log(randomNumber)
-    if(player1Turn){
-        console.log("player 1 rolled" + randomNumber)
-    }else{
-        console.log("player 2 rolled" + randomNumber)
+function showResetButton() {
+    rollBtn.style.display = "none"
+    resetBtn.style.display = "block"
+}
+
+
+ rollBtn.addEventListener("click", function() {
+    const randomNumber = Math.floor(Math.random() * 6) + 1
+
+    if (player1Turn) {
+        player1Score += randomNumber
+        player1Scoreboard.textContent = player1Score
+        player1Dice.textContent = randomNumber
+        player1Dice.classList.remove("active")
+        player2Dice.classList.add("active")
+        message.textContent = "Player 2 Turn"
+        p1Turns += 1
+    } else {
+        player2Score += randomNumber
+        player2Scoreboard.textContent = player2Score
+        player2Dice.textContent = randomNumber
+        player2Dice.classList.remove("active")
+        player1Dice.classList.add("active")
+        message.textContent = "Player 1 Turn"
+        p2Turns += 1
+    }
+    
+    if (player2Score >= 20) {
+        message.textContent = "Player 2 Won 🥳"
+        showResetButton()
+    }  else if (player1Score >= 20 &(p1Turns === p2Turns)) {
+        message.textContent = "Player 1 Won 🎉"
+        showResetButton()
     }
     player1Turn = !player1Turn
 })
+ 
+resetBtn.addEventListener("click", function(){
+    reset()
+})
+
+function reset() {
+    player1Score = 0
+    player2Score = 0
+    player1Turn = true
+    player1Scoreboard.textContent = 0
+    player2Scoreboard.textContent = 0
+    player1Dice.textContent = "-"
+    player2Dice.textContent = "-"
+    message.textContent = "Player 1 Turn"
+    resetBtn.style.display = "none"
+    rollBtn.style.display = "block"
+    player2Dice.classList.remove("active")
+    player1Dice.classList.add("active")
+}
